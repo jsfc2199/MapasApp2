f70c1c63ac5934b283528a318a498e05f9354a61
Juanse Franco <jsfc2199@gmail.com>
2024-07-01 13:28:56 -0500

    dibujando polilinea

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interface';
 import { DirectionsApi } from '../api/directionsApi';
 import { DirectionsResponse, Route } from '../interfaces/directions';
@@ -82,6 +82,46 @@ export class MapService {
 
     this.map?.fitBounds(bounds, {
       padding: 200
+    })
+
+    //Polyline basado en la documentacion de mapbox
+    const sourceData: AnySourceData ={
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties:{},
+            geometry:{
+              type: 'LineString',
+              coordinates: coords
+            }
+          }
+        ]
+      }
+    }
+
+    //limpiamos ruta previa
+    if(this.map?.getLayer('RouteString')){
+      this.map.removeLayer('RouteString')
+      this.map.removeSource('RouteString')
+    }
+
+    this.map?.addSource('RouteString', sourceData)
+
+    this.map?.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout:{
+        'line-cap':'round',
+        "line-join":'round',
+      },
+      paint:{
+        "line-color": 'black',
+        "line-width": 3
+      }
     })
   }
 }
